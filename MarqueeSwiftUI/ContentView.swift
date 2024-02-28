b5ac595c68c96b789320b27ea33a969c8d52c42a
NainaMhrzn <nainamaharjan99@gmail.com>
2024-02-28 10:22:40 +0545

    Initial Commit

---
+++ MarqueeSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarqueeSwiftUI
+//
+//  Created by Naina Maharjan on 28/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarqueeSwiftUI/MarqueeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarqueeSwiftUIApp.swift
+//  MarqueeSwiftUI
+//
+//  Created by Naina Maharjan on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarqueeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

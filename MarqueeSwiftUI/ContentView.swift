5ddc4ad6ac92f71ae1ea8d46b2af93957db4f2e8
NainaMhrzn <nainamaharjan99@gmail.com>
2024-03-13 14:26:02 +0545

    Marquee in SwiftUI

--- MarqueeSwiftUI/ContentView.swift
+++ MarqueeSwiftUI/ContentView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    var velocity: CGFloat = 50
+    let imageArray: [ImageModel] = [.init(imageString: "imageOne", color: .red),
+                                    .init(imageString: "imageTwo", color: .purple),
+                                    .init(imageString: "imageThree", color: .green),
+                                    .init(imageString: "imageFour", color: .blue),
+                                    .init(imageString: "imageFive", color: .mint)
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Marquee(targetVelocity: velocity) {
+                ForEach(imageArray, id: \.color) { i in
+                    VStack {
+                        Image(i.imageString ?? "")
+                    }
+                    .padding()
+                    .frame(width: 260, height: 160)
+                    .background(i.color ?? Color.primary)
+                    .cornerRadius(16)
+                    
+                }
+            }
         }
-        .padding()
+        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
     }
+}
+
+
+struct ImageModel: Identifiable {
+    var id: UUID = UUID()
+    var imageString: String?
+    var color: Color?
 }
 
 #Preview {
